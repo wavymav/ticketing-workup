11a0ca061ef7d8fb514614ef86195a14c30c8e6c
wavymav <mavjbarnes@gmail.com>
2021-01-10 16:58:09 -0800

    auth microservice app setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ import { json } from 'body-parser'
 const app = express()
 app.use(json())
 
+app.get('/api/users/current-user', (req, res) => {
+  res.send('Hello!')
+})
+
 app.listen(3000, () => {
   console.log('Listening on port 3000')
 })
